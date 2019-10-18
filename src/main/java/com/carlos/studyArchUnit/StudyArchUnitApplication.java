6b5a11df12c2a27702454228f925c4f4662898ac
Carlos Chen <carlos.chen@oocl.com>
2019-10-19 00:24:37 +0800

    init

---
+++ src/main/java/com/carlos/studyArchUnit/StudyArchUnitApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.studyArchUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyArchUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyArchUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/studyArchUnit/StudyArchUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.studyArchUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyArchUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
