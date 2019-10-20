4a0e50d84b19f072e99ce04085ac22f45b478284
Carlos Chen <carlos.chen@oocl.com>
2019-10-20 10:30:54 +0800

    Use ArchUnit rule with annotation.

--- src/test/java/com/carlos/studyArchUnit/service/HelloServiceTest.java
+++ src/test/java/com/carlos/studyArchUnit/service/HelloServiceTest.java
@@ -2,21 +2,28 @@ package com.carlos.studyArchUnit.service;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchUnitRunner;
 import com.tngtech.archunit.lang.ArchRule;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(ArchUnitRunner.class) // Remove this line for JUnit 5!!
+@AnalyzeClasses(packages = "com.carlos.studyArchUnit")
 class HelloServiceTest {
+
+    @ArchTest
+    public static final ArchRule myRule = classes()
+            .that().resideInAPackage("..service..")
+            .should().onlyBeAccessed().byAnyPackage("..controller..", "..service..");
 
     @Test
     public void services_should_only_be_accessed_by_Controllers() {
         JavaClasses importedClasses = new ClassFileImporter().importPackages("com.carlos.studyArchUnit");
-
-        ArchRule myRule = classes()
-                .that().resideInAPackage("..service..")
-                .should().onlyBeAccessed().byAnyPackage("..controller..", "..service..");
 
         myRule.check(importedClasses);
     }
